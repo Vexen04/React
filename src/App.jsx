591bfdd7d725b63a5f3f0856c846e5f8b508b35d
Vexen04 <valentin.cardon@orange.fr>
2023-10-12 11:36:15 +0200

    prop Pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 import "./App.css"
 import PokemonCard from "./components/PokemonCard"
 
-function App() {
-  const [count, setCount] = useState(0)
+const pokemonList = [
+  {
+    name: "bulbasaur",
 
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+
+  {
+    name: "mew",
+  },
+]
+
+function App() {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[1]} />
     </div>
   )
 }
